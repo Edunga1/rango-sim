6a4898eab78fd038cc98444fa5cd0623b255964f
edunga1 <goonr21@gmail.com>
2018-02-22 02:51:59 +0900

    Update
    view mode

--- src/app.js
+++ src/app.js
@@ -10,6 +10,11 @@
  * @property {string} price
  * @property {Array.<material>} materials
  */
+/**
+ * @typedef itemView
+ * @property {item} item
+ * @property {number} price total price
+ */
 
 (function () {
     ajax('GET', '/src/data.json', handler);
@@ -19,15 +24,19 @@
 
         /** @type {Array.<item>} */
         var items = data.items;
-        items.forEach(x => {
-            console.log(x.name, price(items, x.id));
-        });
+        /** @type {Array.<itemView>} */
+        var itemViews = items.map(x => ({
+            item: x,
+            price: price(items, x.id)
+        }));
+
+        render(itemViews);
     }
 
     /**
      * @param {Array.<item>} items
      * @param {number} code
-     * @return {number} price
+     * @return {number} total price
      */
     function price(items, code) {
         var item = items.find(x => x.id === code);
@@ -37,5 +46,21 @@
         return item.materials.reduce((pre, x) =>
             pre + (price(items, x.id) * x.count),
             0);
+    }
+
+    /**
+     * @param {Array.<itemView>} items
+     */
+    function render(items) {
+        var html = '<table>';
+        html += items.reduce((pre, x) =>
+            pre + (
+                '<tr>' +
+                    '<td>' + x.item.name + '</td>' +
+                    '<td>' + x.price + '</td>' +
+                '</tr>'),
+            '');
+        html += '</table>';
+        document.querySelector('#content').innerHTML = html;
     }
 })();
