da0dff1047903774bba7da3a1253f2e1dbaf6f1f
edunga1 <goonr21@gmail.com>
2018-02-22 02:15:18 +0900

    Update app.js data.json
    core logic

--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
+/**
+ * @typedef material
+ * @property {number} id
+ * @property {number} count
+ */
+/**
+ * @typedef item
+ * @property {number} id
+ * @property {string} name
+ * @property {string} price
+ * @property {Array.<material>} materials
+ */
+
 (function () {
     ajax('GET', '/src/data.json', handler);
+
     function handler(status, data) {
-        console.log(status, data);
+        if (status !== 200) return;
+
+        /** @type {Array.<item>} */
+        var items = data.items;
+        items.forEach(x => {
+            console.log(x.name, price(items, x.id));
+        });
+    }
+
+    /**
+     * @param {Array.<item>} items
+     * @param {number} code
+     * @return {number} price
+     */
+    function price(items, code) {
+        var item = items.find(x => x.id === code);
+
+        if (!item) return 0;
+        if (!Array.isArray(item.materials) || !item.materials.length) return item.price || 0;
+        return item.materials.reduce((pre, x) =>
+            pre + (price(items, x.id) * x.count),
+            0);
     }
 })();
